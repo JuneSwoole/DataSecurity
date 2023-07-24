680463906173b4391957a2ae14a3f146b4025572
June Chen <junechen_0606@163.com>
2023-07-24 10:49:19 +0800

    refactor: update namespace

--- src/Singleton.php
+++ src/Singleton.php
@@ -3,13 +3,13 @@
  * @Author: juneChen && juneswoole@163.com
  * @Date: 2023-07-21 10:13:16
  * @LastEditors: juneChen && juneswoole@163.com
- * @LastEditTime: 2023-07-24 10:41:18
+ * @LastEditTime: 2023-07-24 10:48:15
  * 
  */
 
 declare(strict_types=1);
 
-namespace june\DataSecurity;
+namespace June\DataSecurity;
 
 trait Singleton
 {
